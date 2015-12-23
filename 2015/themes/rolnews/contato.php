2659f5c012c1a070f55f3dd584925058479d53ee
Gean <Gean@SERVIDOR>
2015-12-23 12:10:27 -0400

    Update - v.1.0.45 --Ajustes

--- 2015/themes/rolnews/contato.php
+++ 2015/themes/rolnews/contato.php
@@ -3,7 +3,7 @@
         <div class="vinVideo borderBottomBlue t18 grafite bold">ENTRE EM CONTATO COM O ROLNEWS</div>
         <div class="breadcrumb t16">
             <p>O ROLNEWS  é uma empresa de comunicação, voltada para o segmento de notícias e entretenimento digital e impresso.</p>
-            <p>Criada em 12 de Maio de 2005, o ROLNEWS se tornou um dos principais portais de Ronônia.</p>
+            <p>Criada em 12 de Maio de 2005, o ROLNEWS se tornou um dos principais portais de Rondônia.</p>
             <br>
             <p><b>Telefone</b><br> <b class="glyphicon glyphicon-phone"></b> (69) 8411-0434</p>
             <p><b>E-mails</b><br>
